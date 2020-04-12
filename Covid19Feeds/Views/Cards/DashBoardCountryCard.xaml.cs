003c255ed8134d5d2fbd562766ba311d7ba4a8e6
KiranKumar <kiran.kumarb@outlook.com>
2020-04-12 17:43:41 +0530

    DashBoard UI Design

---
+++ Covid19Feeds/Views/Cards/DashBoardCountryCard.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Covid19Feeds.Views.Cards
+{
+    public partial class DashBoardCountryCard : ContentView
+    {
+        public DashBoardCountryCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
