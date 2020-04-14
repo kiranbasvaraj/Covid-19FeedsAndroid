b72562513babe3fa06a6889d21f2af5fbae02b99
KiranKumar <kiran.kumarb@outlook.com>
2020-04-15 01:13:04 +0530

    Integrated Resources Page

--- Covid19Feeds/Views/ContentViews/AboutView.xaml.cs
+++ Covid19Feeds/Views/ContentViews/AboutView.xaml.cs
@@ -10,5 +10,25 @@ namespace Covid19Feeds.Views.ContentViews
         {
             InitializeComponent();
         }
+
+        public async void OnResourceTapped(object s,EventArgs e)
+        {
+            await Navigation.PushAsync(new ResourcesUsedPage());
+        }
+
+        public async void OnFeedbackTapped(object s, EventArgs e)
+        {
+        }
+
+
+        public async void OnShareTapped(object s, EventArgs e)
+        {
+        }
+
+
+        public async void OnAboutmeTapped(object s, EventArgs e)
+        {
+        }
+
     }
 }
---
+++ Covid19Feeds/Views/ResourcesUsedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Covid19Feeds.Views.ContentViews
+{
+    public partial class ResourcesUsedPage : ContentPage
+    {
+        public ResourcesUsedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
