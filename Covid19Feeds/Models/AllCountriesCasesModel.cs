5c106077b361e26749061c0734bedb6c61b4861d
KiranKumar <kiran.kumarb@outlook.com>
2020-09-05 12:42:18 +0530

    API serialisation fix

--- Covid19Feeds/Models/AllCountriesCasesModel.cs
+++ Covid19Feeds/Models/AllCountriesCasesModel.cs
@@ -6,12 +6,14 @@ namespace Covid19Feeds.Models
    
     public class CountryInfo
     {
-        public int? _id { get; set; }
+        public string _id { get; set; }
         public string iso2 { get; set; }
         public string iso3 { get; set; }
-        public double? lat { get; set; }
-        public double? @long { get; set; }
+        public string lat { get; set; }
+        public string @long { get; set; }
         public string flag { get; set; }
+
+
     }
 
     public class AllCountriesCasesModel
@@ -19,17 +21,25 @@ namespace Covid19Feeds.Models
         public object updated { get; set; }
         public string country { get; set; }
         public CountryInfo countryInfo { get; set; }
-        public int? cases { get; set; }
-        public int? todayCases { get; set; }
-        public int? deaths { get; set; }
-        public int? todayDeaths { get; set; }
-        public int? recovered { get; set; }
-        public int? active { get; set; }
-        public int? critical { get; set; }
-        public int? casesPerOneMillion { get; set; }
-        public int? deathsPerOneMillion { get; set; }
-        public int? tests { get; set; }
-        public int? testsPerOneMillion { get; set; }
+        public string cases { get; set; }
+        public string todayCases { get; set; }
+        public string deaths { get; set; }
+        public string todayDeaths { get; set; }
+        public string recovered { get; set; }
+        public string active { get; set; }
+        public string critical { get; set; }
+        public string casesPerOneMillion { get; set; }
+        public string deathsPerOneMillion { get; set; }
+        public string tests { get; set; }
+        public string testsPerOneMillion { get; set; }
+        public string population { get; set; }
+        public string continent { get; set; }
+        public string oneCasePerPeople { get; set; }
+        public string oneDeathPerPeople { get; set; }
+        public string oneTestPerPeople { get; set; }
+        public string activePerOneMillion { get; set; }
+        public string recoveredPerOneMillion { get; set; }
+        public string criticalPerOneMillion { get; set; }
 
         [JsonIgnore]
         public string StartColor { get; set; }
